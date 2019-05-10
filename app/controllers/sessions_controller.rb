c5a0910668266e90508d74e988590ecb789544da
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-10 17:31:03 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,9 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: params[:name])
+    byebug
     return head(:forbidden) unless @user && @user.authenticate(params[:password])
     session[:user_id] = @user.id
-    binding.pry
   end
 
   def destroy
